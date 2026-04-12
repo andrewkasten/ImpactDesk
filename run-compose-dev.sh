fbff68b995e2dacb5aeb96936d6086b4313bd4d2
and3k <andrewjkasten@gmail.com>
2026-04-12 09:22:09 -0600

    start scripts for dev and prod

--- run-compose-dev.sh
+++ run-compose-dev.sh
@@ -1,5 +1,7 @@
-set -e
 
 docker compose -f docker-compose.dev.yml down -v
-docker compose --progress plain -f docker-compose.dev.yml build --no-cache 
-docker compose -f docker-compose.dev.yml up -d db api frontend
+docker compose -f docker-compose.dev.yml build --no-cache
+docker compose -f docker-compose.dev.yml up -d
+
+sleep 5
+docker exec impactdesk-api-1 python manage.py migrate
